33855cea4280d54bb31a18fab8da55e3a762e50c
Hexicube <marc@firstlinetech.co.uk>
2020-02-04 23:30:09 +0000

    Oxygen Not Included - Image to Wallpaper BP

---
+++ ONI Wallpaper BP/cols.js
@@ -0,0 +1,336 @@
+const materials = {
+	'-2070223827': {
+		name: 'Aerogel',
+		col: [0, 217, 255],
+		unobtainable: true
+	},
+	'-1870043872': {
+		name: 'Algae',
+		col: [0, 255, 47]
+	},
+	2108244480: {
+		name: 'Aluminum',
+		col: [127, 181, 202]
+	},
+	167973730: {
+		name: 'Aluminum Ore',
+		col: [204, 67, 36]
+	},
+	1433229102: {
+		name: 'Bitumen',
+		col: [77, 77, 77]
+	},
+	'-839728230': {
+		name: 'Bleach Stone',
+		col: [0, 161, 255]
+	},
+	'-325269471': {
+		name: 'Brick',
+		col: [250, 35, 0],
+		unobtainable: true
+	},
+	'-1561279013': {
+		name: 'Brine Ice',
+		col: [0, 238, 255]
+	},
+	947100397: {
+		name: 'Coal',
+		col: [195, 167, 209]
+	},
+	118518245: {
+		name: 'Carbon Fiber',
+		col: [96, 96, 96],
+		unobtainable: true
+	},
+	1627140480: {
+		name: 'Cement',
+		col: [149, 149, 149],
+		unobtainable: true
+	},
+	1668719292: {
+		name: 'Cement Mix',
+		col: [161, 161, 161],
+		unobtainable: true
+	},
+	'-1467370314': {
+		name: 'Ceramic',
+		col: [236, 214, 174]
+	},
+	867327137: {
+		name: 'Clay',
+		col: [153, 156, 85]
+	},
+	'-1725038055': {
+		name: 'Copper',
+		col: [255, 156, 8]
+	},
+	976099455: {
+		name: 'Genetic Ooze',
+		col: [71, 69, 6],
+		unobtainable: true
+	},
+	'-2123557039': {
+		name: 'Crushed Ice',
+		col: [97, 219, 255],
+		unobtainable: true
+	},
+	'-1714565729': {
+		name: 'Crushed Rock',
+		col: [140, 140, 140]
+	},
+	'-1736594426': {
+		name: 'Copper Ore',
+		col: [217, 91, 99]
+	},
+	'-2079931820': {
+		name: 'Diamond',
+		col: [203, 197, 233]
+	},
+	1624244999: {
+		name: 'Dirt',
+		col: [171, 125, 0]
+	},
+	1664334585: {
+		name: 'Polluted Ice',
+		col: [126, 204, 214]
+	},
+	28407099: {
+		name: 'Electrum',
+		col: [52, 110, 243],
+		unobtainable: true
+	},
+	'-1396791454': {
+		name: 'Fertilizer',
+		col: [42, 173, 229]
+	},
+	2059777261: {
+		name: 'Pyrite',
+		col: [255, 163, 13],
+		unobtainable: true
+	},
+	1757792140: {
+		name: 'Fossil',
+		col: [255, 50, 0]
+	},
+	245514112: {
+		name: 'Fullerene',
+		col: [63, 63, 63]
+	},
+	623986332: {
+		name: 'Glass',
+		col: [57, 255, 213]
+	},
+	'-279785280': {
+		name: 'Gold',
+		col: [255, 201, 26]
+	},
+	361868060: {
+		name: 'Gold Amalgam',
+		col: [255, 201, 26]
+	},
+	'-105943486': {
+		name: 'Granite',
+		col: [67, 67, 67]
+	},
+	873952427: {
+		name: 'Ice',
+		col: [0, 238, 255]
+	},
+	'-355957251': {
+		name: 'Igneous Rock',
+		col: [118, 118, 118]
+	},
+	1306370440: {
+		name: 'Iron',
+		col: [204, 67, 36]
+	},
+	1608833498: {
+		name: 'Iron Ore',
+		col: [204, 67, 36]
+	},
+	'-2008682336': {
+		name: 'Isoresin',
+		col: [255, 163, 0]
+	},
+	1071649902: {
+		name: 'Abyssalite',
+		col: [50, 49, 77]
+	},
+	'-755153220': {
+		name: 'Lead',
+		col: [255, 201, 26]
+	},
+	'-721320011': {
+		name: 'Lime',
+		col: [255, 255, 255]
+	},
+	1282846257: {
+		name: 'Mafic Rock',
+		col: [163, 158, 173]
+	},
+	'-1779895821': {
+		name: 'Niobium',
+		col: [94, 253, 173]
+	},
+	'-474151749': {
+		name: 'Obsidian',
+		col: [19, 20, 24]
+	},
+	1262005685: {
+		name: 'Oxylite',
+		col: [4, 132, 134]
+	},
+	'-1901832310': {
+		name: 'Phosphate Nodules',
+		col: [195, 41, 115],
+		unobtainable: true
+	},
+	'-877427037': {
+		name: 'Phosphorite',
+		col: [200, 61, 128]
+	},
+	'-220394187': {
+		name: 'Phosphorus',
+		col: [200, 61, 128]
+	},
+	'-1142341158': {
+		name: 'Plastic',
+		col: [0, 229, 255]
+	},
+	'-47820500': {
+		name: 'Radium',
+		col: [129, 176, 0],
+		unobtainable: true
+	},
+	'-902240476': {
+		name: 'Refined Carbon',
+		col: [67, 67, 67]
+	},
+	1362238252: {
+		name: 'Regolith',
+		col: [149, 118, 173]
+	},
+	'-233232444': {
+		name: 'Rust',
+		col: [188, 80, 79]
+	},
+	381665462: {
+		name: 'Salt',
+		col: [255, 255, 255]
+	},
+	381796644: {
+		name: 'Sand',
+		col: [207, 140, 42]
+	},
+	'-1038746460': {
+		name: 'Sand Cement',
+		col: [204, 174, 130],
+		unobtainable: true
+	},
+	493438017: {
+		name: 'Sandstone',
+		col: [152, 130, 53]
+	},
+	183408504: {
+		name: 'Sedimentary Rock',
+		col: [70, 114, 204]
+	},
+	1412740729: {
+		name: 'Building Slab',
+		col: [122, 140, 180],
+		unobtainable: true
+	},
+	'-1153056158': {
+		name: 'Slime',
+		col: [144, 214, 92]
+	},
+	489261827: {
+		name: 'Snow',
+		col: [180, 209, 255]
+	},
+	83003332: {
+		name: 'Carbon Dioxide',
+		col: [31, 31, 31]
+	},
+	'-690060127': {
+		name: 'Chlorine',
+		col: [162, 221, 16]
+	},
+	'-1224086026': {
+		name: 'Crude Oil',
+		col: [0, 0, 0]
+	},
+	'-858172533': {
+		name: 'Hydrogen',
+		col: [103, 5, 76]
+	},
+	'-537625624': {
+		name: 'Mercury',
+		col: [49, 72, 176],
+		unobtainable: true
+	},
+	1183979137: {
+		name: 'Methane',
+		col: [137, 110, 34]
+	},
+	'-1112594153': {
+		name: 'Naphtha',
+		col: [214, 197, 183]
+	},
+	973502379: {
+		name: 'Oxygen',
+		col: [89, 234, 226]
+	},
+	'-473261502': {
+		name: 'Petroleum',
+		col: [229, 233, 10]
+	},
+	166493482: {
+		name: 'Propane',
+		col: [121, 144, 209]
+	},
+	'-389019570': {
+		name: 'Super Coolant',
+		col: [55, 173, 147]
+	},
+	'-1495120499': {
+		name: 'Visco-Gel',
+		col: [79, 16, 183]
+	},
+	'-899253461': {
+		name: 'Steel',
+		col: [81, 82, 43]
+	},
+	'-729385479': {
+		name: 'Sulfur',
+		col: [255, 255, 0]
+	},
+	'-1713958528': {
+		name: 'Insulation',
+		col: [255, 0, 207]
+	},
+	1559722904: {
+		name: 'Thermium',
+		col: [210, 157, 168]
+	},
+	869554203: {
+		name: 'Polluted Dirt',
+		col: [14, 96, 0]
+	},
+	'-1058835580': {
+		name: 'Tungsten',
+		col: [255, 247, 0]
+	},
+	1838482828: {
+		name: 'Neutronium',
+		col: [255, 0, 255]
+	},
+	'-1208854194': {
+		name: 'Wolframite',
+		col: [137, 183, 255]
+	},
+	1937241528: {
+		name: 'Ethanol',
+		col: [0, 255, 106]
+	}
+};
