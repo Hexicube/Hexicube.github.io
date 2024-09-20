83568079e1353f8869c0a663075f16f2265a37fc
Hexicube <marc@firstlinetech.co.uk>
2024-09-21 00:16:33 +0100

    8BP: Shots are now sorted by target X position

--- 8BP/pool.js
+++ 8BP/pool.js
@@ -3,16 +3,16 @@ var shotInfo = {
     {
       "tint": "grey",
       "desc": "Slightly too hard.",
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.58, .59],
+      "cue":    [.30, .77],
+      "gold":   [.36, .57]
     },
     {
       "tint": "gold",
       "desc": "As shown.",
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.45, .55],
+      "cue":    [.75, .64],
+      "gold":   [.69, .50]
     },
     {
       "tint": "red",
@@ -20,16 +20,16 @@ var shotInfo = {
         "Slightly too hard.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.38, .46],
+      "cue":    [.65, .61],
+      "gold":   [.61, .48]
     },
     {
       "tint": "gold",
       "desc": "As shown (very slightly too soft).",
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.64, .48],
+      "cue":    [.30, .71],
+      "gold":   [.34, .55]
     },
     {
       "tint": "red",
@@ -37,9 +37,9 @@ var shotInfo = {
         "Black section halfway over line?",
         "Barely too hard."
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.63, .67],
+      "cue":    [.38, .34],
+      "gold":   [.40, .45]
     },
     {
       "tint": "white",
@@ -47,9 +47,9 @@ var shotInfo = {
         "Add backspin to avoid double-kissing.",
         "Too soft."
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.57, .48],
+      "cue":    [.75, .81],
+      "gold":   [.74, .73]
     },
     {
       "tint": "cyan",
@@ -57,23 +57,23 @@ var shotInfo = {
         "Too soft.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.48, .65],
+      "cue":    [.30, .34],
+      "gold":   [.31, .43]
     },
     {
       "tint": "gold",
       "desc": "",
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.73, .51],
+      "cue":    [.36, .67],
+      "gold":   [.50, .63]
     },
     {
       "tint": "gold",
       "desc": "",
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.67, .53],
+      "cue":    [.30, .47],
+      "gold":   [.38, .63]
     },
     {
       "tint": "white",
@@ -81,9 +81,9 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.43, .62],
+      "cue":    [.76, .75],
+      "gold":   [.71, .61]
     },
     {
       "tint": "red",
@@ -91,9 +91,9 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.58, .71],
+      "cue":    [.34, .41],
+      "gold":   [.41, .35]
     },
     {
       "tint": "white",
@@ -101,9 +101,9 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.66, .67],
+      "cue":    [.44, .43],
+      "gold":   [.52, .39]
     },
     {
       "tint": "red",
@@ -111,9 +111,9 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.46, .49],
+      "cue":    [.66, .76],
+      "gold":   [.55, .78]
     },
     {
       "tint": "grey",
@@ -121,9 +121,9 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.58, .62],
+      "cue":    [.75, .35],
+      "gold":   [.74, .45]
     },
     {
       "tint": "grey",
@@ -131,9 +131,9 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.68, .55],
+      "cue":    [.30, .69],
+      "gold":   [.38, .50]
     }
   ],
   "chain": [
@@ -143,10 +143,10 @@ var shotInfo = {
         "Too soft.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.66, .55],
+      "cue":    [.29, .52],
+      "gold":   [.47, .43],
+      "red":    [.37, .44]
     },
     {
       "tint": "grey",
@@ -154,10 +154,10 @@ var shotInfo = {
         "Far too hard, probably intended for force 1.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.72, .49],
+      "cue":    [.38, .84],
+      "gold":   [.53, .54],
+      "red":    [.46, .66]
     },
     {
       "tint": "cyan",
@@ -165,10 +165,10 @@ var shotInfo = {
         "Slightly too soft.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.37, .69],
+      "cue":    [.74, .41],
+      "gold":   [.51, .45],
+      "red":    [.62, .41]
     },
     {
       "tint": "gold",
@@ -176,10 +176,10 @@ var shotInfo = {
         "Hit between shown and 1/4 power.",
         "Shown power almost too hard."
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.34, .53],
+      "cue":    [.70, .47],
+      "gold":   [.55, .45],
+      "red":    [.62, .45]
     },
     {
       "tint": "cyan",
@@ -187,10 +187,10 @@ var shotInfo = {
         "Too hard.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.68, .57],
+      "cue":    [.34, .49],
+      "gold":   [.50, .47],
+      "red":    [.43, .46]
     },
     {
       "tint": "grey",
@@ -198,10 +198,10 @@ var shotInfo = {
         "Too hard (bounces).",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.67, .65],
+      "cue":    [.28, .58],
+      "gold":   [.52, .68],
+      "red":    [.35, .63]
     },
     {
       "tint": "cyan",
@@ -209,10 +209,10 @@ var shotInfo = {
         "Too soft, cut too far.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.52, .45],
+      "cue":    [.79, .84],
+      "gold":   [.72, .61],
+      "red":    [.76, .71]
     },
     {
       "tint": "cyan",
@@ -220,18 +220,18 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.34, .52],
+      "cue":    [.77, .60],
+      "gold":   [.51, .67],
+      "red":    [.62, .67]
     },
     {
       "tint": "gold",
       "desc": "",
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.69, .67],
+      "cue":    [.35, .34],
+      "gold":   [.55, .43],
+      "red":    [.45, .36]
     },
     {
       "tint": "grey",
@@ -239,10 +239,10 @@ var shotInfo = {
         "Too soft, bad angle.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.34, .69],
+      "cue":    [.77, .42],
+      "gold":   [.58, .66],
+      "red":    [.68, .58]
     },
     {
       "tint": "red",
@@ -250,10 +250,10 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.36, .56],
+      "cue":    [.68, .62],
+      "gold":   [.54, .65],
+      "red":    [.61, .66]
     },
     {
       "tint": "grey",
@@ -261,10 +261,10 @@ var shotInfo = {
         "Cut far too much.",
         "Red barely hits gold."
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.58, .52],
+      "cue":    [.29, .55],
+      "gold":   [.40, .49],
+      "red":    [.36, .51]
     },
     {
       "tint": "grey",
@@ -272,10 +272,10 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.51, .70],
+      "cue":    [.25, .31],
+      "gold":   [.32, .54],
+      "red":    [.28, .43]
     },
     {
       "tint": "gold",
@@ -283,10 +283,10 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": [],
-      "red": []
+      "target": [.54, .69],
+      "cue":    [.79, .29],
+      "gold":   [.67, .39],
+      "red":    [.73, .33]
     }
   ],
   "rebound": [
@@ -296,9 +296,9 @@ var shotInfo = {
         "Aim with high aim level queue to line up, align battered cue to blue/white border on table cushion.",
         "Almost too soft, aim for 1/3rd of black segment below line."
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.51, .66],
+      "cue":    [.50, .36],
+      "gold":   [.35, .39]
     },
     {
       "tint": "cyan",
@@ -308,9 +308,9 @@ var shotInfo = {
         "Off target, too soft.",
         "End result of C."
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.34, .67],
+      "cue":    [.33, .35],
+      "gold":   [.57, .35]
     },
     {
       "tint": "grey",
@@ -318,9 +318,9 @@ var shotInfo = {
         "On target, far too hard.",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.47, .69],
+      "cue":    [.68, .46],
+      "gold":   [.56, .43]
     },
     {
       "tint": "grey",
@@ -328,9 +328,9 @@ var shotInfo = {
         "On target, do not adjust aim.",
         "Far too hard."
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.36, .64],
+      "cue":    [.48, .42],
+      "gold":   [.62, .45]
     },
     {
       "tint": "gold",
@@ -338,18 +338,32 @@ var shotInfo = {
         "",
         ""
       ],
-      "target": [],
-      "cue": [],
-      "gold": []
+      "target": [.69, .69],
+      "cue":    [.71, .38],
+      "gold":   [.48, .40]
     }
   ]
 };
 
+function getPos(e) {
+  var posX = ((e.x - e.currentTarget.x) / e.currentTarget.clientWidth);
+  var posY = ((e.y - e.currentTarget.y) / e.currentTarget.clientHeight);
+  //alert("x: " + posX + "\ny: " + posY);
+  return [posX, posY];
+}
+
+function shotSort(a, b) {
+  if (a.target[0] < b.target[0]) return -1;
+  if (a.target[0] > b.target[0]) return 1;
+  return 0;
+}
+
 function iterateShots(data, type) {
-  data.forEach((shot, i) => {
-    var name = type + " " + (i+1);
+  data.forEach((shot, i) => { shot.imageID = i+1; });
+  var sortedData = data.sort(shotSort);
+  sortedData.forEach((shot, i) => {
+    var name = type + " " + shot.imageID;
     var shotHtml = buildShot(shot, name);
-    console.log(shotHtml);
 
     var shotObj = document.createElement("div");
     shotObj.id = name;
@@ -377,7 +391,7 @@ function buildShot(data, name) {
   var resp = "<h3>" + name + "</h3><div style='display: flex; flex-wrap: wrap'>";
   var firstImg = "<img src='./" + images[0][0] + ".png' width=100% />";
   images.forEach(img => {
-    resp += "<div style='flex-basis:50%; margin-top: 20px'><img src='./" + img[0] + ".png' width=100% /><br/>";
+    resp += "<div style='flex-basis:50%; margin-top: 20px'><img src='./" + img[0] + ".png' width=100% onclick='getPos(event)' /><br/>";
     resp += "<span style='margin-left: 40px'>" + img[1] + "</span></div>";
   });
   return [resp + "</div>", firstImg];
